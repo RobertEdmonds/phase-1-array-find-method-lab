f266692db123958c4b8f9d71e140fd315c16e6b6
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-05 15:03:05 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+const record = [
+    {year:'2018', result: 'L'},
+    {year:'2017', result: 'W'},
+    {year:'2016', result: 'N/A'}
+]
+function superbowlWin(array){
+    const winningTeam = array.find(wins => wins.result === 'W')
+    if (winningTeam){
+        return winningTeam.year
+    }
+}
+    
